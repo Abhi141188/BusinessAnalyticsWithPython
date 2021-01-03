9a91d660c1ca8acdf9db25db0491d7117466e72f
abhinav choudhary <55540924+Abhi141188@users.noreply.github.com>
2021-01-03 22:49:09 +0530

    Seq No. 24

--- TM_Twitter_Analysis.py
+++ TM_Twitter_Analysis.py
@@ -11,14 +11,14 @@ from textblob import TextBlob
 #Twitter Authentication
 consumer_key = 'zGyB4DjnB32mKpKxZaF4M2PnB'
 consumer_secret = 'DsV7gfgJxOBV0wgSq4UN5j3yYNOlsrSxGw2xhF7Kz0JQHeZ6AF'
-access_token = '1212320988563558406-0sFKPepwoHaqPwG2sTwDSLwye5uR7B'
-access_token_secret = '2cAbGlJSnGZHWXngp5UR3MsVjfR0tZomAgEcsCNtDAAcN'
+access_token = '1212320988563558406-oFECPAuTKJQ7cMYGkAeptJ0R1rbu4v'
+access_token_secret = '5kivnKzSVyE3B2CoT1UrHaAQ8SM7tGhSUe9ZsPd6xQSAG'
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 #Reading data from twitter
 api = tweepy.API(auth)
-public_tweets = api.search('India')
+public_tweets = api.search('China')
 
 #Printing the data read from twitter
 for tweet in public_tweets:
