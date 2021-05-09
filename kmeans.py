d98ea9efcb26c5f4c0981ce8d4cb6fd86f8d8f54
abhinav choudhary <55540924+Abhi141188@users.noreply.github.com>
2021-05-09 21:14:13 +0530

    Seq No. 17

--- kmeans.py
+++ kmeans.py
@@ -7,14 +7,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('F:/pyWork/pyData/Mall_Customers.csv')
+dataset = pd.read_csv('F:/WORK/pyWork/pyData/Mall_Customers.csv')
 X = dataset.iloc[:, [3,4]].values
 
 # Using the elbow method to find the optimal number of clusters
 from sklearn.cluster import KMeans
 wcss = []
 for i in range(1, 11):
-    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 0)
     kmeans.fit(X)
     wcss.append(kmeans.inertia_)
 plt.plot(range(1, 11), wcss)
